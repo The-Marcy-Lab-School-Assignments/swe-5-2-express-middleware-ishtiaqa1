fef192357a9a5dcbf3786210d0f48055eca0a0ec
ishtiaq <ishtiaqa@buffalo.edu>
2026-03-04 16:59:13 -0500

    Complete express and middleware assignment

--- server/index.js
+++ server/index.js
@@ -27,29 +27,56 @@ const quotes = [
 
 // TODO: Register middleware with app.use() before the controllers
 
+const logRoutes = (req, res, next) => {
+  const time = new Date().toLocaleString();
+  console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+  next();
+};
 
+app.use(logRoutes);
+
+const pathToFrontend = path.join(__dirname, '../frontend');
+const serveStatic = express.static(pathToFrontend);
+app.use(serveStatic);
 
 // TODO: Define controllers here
 
 // listQuotes — sends all quotes as JSON
 //   If the request includes a ?topic= query string, send only quotes with a matching topic
 
+const listQuotes = (req, res, next) => {
+  const { topic } = req.query;
+  if (topic) return res.json(quotes.filter(t => t.topic === topic));
+  res.json(quotes);
+}
+
 // getQuote — sends a single quote whose id matches req.params.id
 //   If no matching quote is found, respond with 404 and { error: 'No quote with id <id>' }
 
-
+const getQuote = (req, res, next) => {
+  const { id } = req.params;
+  const content = quotes.find(q => q.id === Number(id));
+  if (!content) return res.status(404).json({ error: `No quote with id ${id}` });
+  res.json(content);
+}
 
 // TODO: Register endpoints here
 
 // GET /api/quotes
 // GET /api/quotes/:id
 
+app.get('/api/quotes',listQuotes);
+app.get('/api/quotes/:id', getQuote);
 
 
 // TODO: Add a catch-all fallback that responds with 404 and { error: 'Not found: <url>' }
 // Use app.use() and place it after all other routes
 
+const serve404 = (req, res, next) => {
+  res.status(404).send({ error: `Not found: ${req.originalUrl}` });
+}
 
+app.use(serve404);
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
